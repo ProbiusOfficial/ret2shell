7575d69ec45eecd4807ac7383d232c6126d2a3f4
Reverier-Xu <reverier.xu@outlook.com>
2023-08-17 19:30:25 +0800

    :construction: init page

---
+++ web/src/lib/models/action.ts
@@ -0,0 +1,8 @@
+export interface Action {
+    id: number
+    created_at: number
+    started_at: number
+    challenge_id: number
+    status: number
+    commit_id: string
+}
---
+++ web/src/lib/models/answer.ts
@@ -0,0 +1,9 @@
+export interface Answer {
+    id: number
+    title: string
+    published_at: number
+    updated_at: number
+    author_id: number
+    challenge_id: number
+    content: string
+}
